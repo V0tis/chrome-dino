6e8bc6547d96474fb3579de2214a02227228d347
JungDonggun <donggun.dev@gmail.com>
2022-04-05 19:11:14 +0900

    add config for cactus

---
+++ src/config/cactus.js
@@ -0,0 +1,14 @@
+class Cactus {
+  constructor() {
+    this.x = 500;
+    this.y = 200;
+    this.width = 50;
+    this.height = 50;
+  }
+
+  draw() {
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+}
+
